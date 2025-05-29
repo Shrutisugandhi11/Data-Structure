71c8fdc0a7734c4cac2cebaf8e25a68d261be643
ShrutiSugandhi <shrutisugandhi00@gmail.com>
2025-05-30 00:14:07 +0530

    Basics of Recursion part-III

--- Recursion/src/main/java/org/data/structure/recursion/basics/RecursionProblem3.java
+++ Recursion/src/main/java/org/data/structure/recursion/basics/RecursionProblem3.java
@@ -14,15 +14,30 @@ public class RecursionProblem3 {
     }
 
     /**
-     *
-     *
+     *2.Check if string is palindrome or not.
      */
+    public static boolean isPalindrome(String string,int start,int end){
 
-    public static void main(String[] args) {
+        if(start>=end/2) return true;
+        if(string.charAt(start)!=string.charAt(end-start-1)) return false;
+        return isPalindrome(string, start+1, end);
+    }
+
+    /**
+     * 3.Fibonacii series
+     */
+    public static int fibonaciiSeries(int N){
+        if (N<=1) return N;
+        return fibonaciiSeries(N-1)+fibonaciiSeries(N-2);
+    }
+     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
         reverseArray(arr, 0, 4);
         for(int i:arr){
             System.out.println(""+i+", ");
         }
+        System.out.println(isPalindrome("shrutiiturhs",0,12));
+
+         System.out.println(fibonaciiSeries(4));
     }
 }
