1c36c5ca0082147137b5752ff8c817d33d3918ff
ShrutiSugandhi <shrutisugandhi00@gmail.com>
2025-06-21 00:33:54 +0530

    Added utility class.

---
+++ Utilites/DataUtils.java
@@ -0,0 +1,12 @@
+package Utilites;
+
+public class DataUtils {
+    public static int[] emptyArr = {};
+
+    public static int[] mixedArr = {5, -2, 0, 99, -10, 8};
+
+    public static final int[] testArr = {10, 2, 33, 2, 18, 7, 0, 10};
+
+    public static int[] reversedArr = {9, 7, 5, 3, 1};
+
+}
