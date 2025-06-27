43b264541adcab4e3015f0eacbdeb2be0a056d67
ShrutiSugandhi <shrutisugandhi00@gmail.com>
2025-06-27 19:00:37 +0530

    Fixes duplicate element sorting

--- SortingTechniques/QuickSort.java
+++ SortingTechniques/QuickSort.java
@@ -6,6 +6,12 @@ import java.util.Arrays;
 
 import static Utilites.DataUtils.swap;
 
+/**
+ * Quick Sort: pick a pivot, with the help of two pointers low and high sort smaller elements in the left;
+ * and larger elements to right.
+ * once sorting is done place pivot to its right position
+ */
+
 public class QuickSort {
     public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
@@ -16,17 +22,19 @@ public class QuickSort {
     }
 
     private static int partition(int[] arr, int low, int high) {
-        int pivot = low;
+        int pivot = arr[low];
         int i = low;
         int j = high;
         while (i < j) {
-            while (arr[pivot] > arr[i] && i<=high)
+            while (i <= high && arr[i] <= pivot)
                 i++;
-            while (arr[pivot] < arr[j] && j>=low)
+            while (j > low && arr[j] >= pivot)
                 j--;
-            swap(arr, i, j);
+            if (i < j)
+                swap(arr, i, j);
         }
-        swap(arr, pivot, j);
+
+        swap(arr, low, j);
         pivot = j;    //assign j to pivot as pivot was low
         return pivot;
     }
